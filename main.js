551555e34a994e10b8b76f97de2455dc3603ed90
Leo Chien <leoch@example.com>
2024-07-14 18:10:09 +0800

    Update Webpack entry point and initial commit

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
     $('#accesscamera').on('click', function() {
         Webcam.reset();
         Webcam.on('error', function() {
-            //$('#photoModal').modal('hide');
             swal({
                 title: 'Warning',
                 text: 'Please give permission to access your webcam',
@@ -23,19 +22,11 @@ $(document).ready(function() {
     $('#takephoto').on('click', take_snapshot);
 
     $('#retakephoto').on('click', function() {
-        $('#my_camera').addClass('d-block');
-        $('#my_camera').removeClass('d-none');
-
+        $('#my_camera').addClass('d-block').removeClass('d-none');
         $('#results').addClass('d-none');
-
-        $('#takephoto').addClass('d-block');
-        $('#takephoto').removeClass('d-none');
-
-        $('#retakephoto').addClass('d-none');
-        $('#retakephoto').removeClass('d-block');
-
-        $('#uploadphoto').addClass('d-none');
-        $('#uploadphoto').removeClass('d-block');
+        $('#takephoto').addClass('d-block').removeClass('d-none');
+        $('#retakephoto').addClass('d-none').removeClass('d-block');
+        $('#uploadphoto').addClass('d-none').removeClass('d-block');
     });
 
     $('#uploadphoto').on('click', function() {
@@ -43,53 +34,45 @@ $(document).ready(function() {
         upload_photo(raw_image_data);
     });
 
-    function upload_photo(raw_image_data) {
-        fetch('https://app-bq9j.onrender.com', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ image: raw_image_data })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.result >3) {
+    async function upload_photo(raw_image_data) {
+        try {
+            const response = await fetch('https://app-bq9j.onrender.com', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ image: raw_image_data })
+            });
+            const data = await response.json();
+            if (data.result > 3) {
                 $('#noStrabismusModal').modal('show');
-            }  else {
+            } else {
                 $('#noStrabismusModal').modal('show');
             }
             console.log('Solution:', data.solution);
-        })
-        .catch(error => {
+        } catch (error) {
             console.error('Error:', error);
             swal({
                 title: 'Error',
                 text: 'Failed to upload photo. Please try again.',
                 icon: 'error'
             });
+        }
+    }
+
+    function take_snapshot() {
+        Webcam.snap(function(data_uri) {
+            $('#results').html('<img src="' + data_uri + '" class="d-block mx-auto rounded"/>');
+
+            var raw_image_data = data_uri.replace(/^data:image\/\w+;base64,/, '');
+            $('#photoStore').val(raw_image_data);
         });
+
+        $('#my_camera').removeClass('d-block').addClass('d-none');
+        $('#results').removeClass('d-none');
+        $('#takephoto').removeClass('d-block').addClass('d-none');
+        $('#retakephoto').removeClass('d-none').addClass('d-block');
+        $('#uploadphoto').removeClass('d-none').addClass('d-block');
     }
 });
 
-function take_snapshot() {
-    Webcam.snap(function(data_uri) {
-        $('#results').html('<img src="' + data_uri + '" class="d-block mx-auto rounded"/>');
-
-        var raw_image_data = data_uri.replace(/^data\:image\/\w+\;base64\,/, '');
-        $('#photoStore').val(raw_image_data);
-    });
-
-    $('#my_camera').removeClass('d-block');
-    $('#my_camera').addClass('d-none');
-
-    $('#results').removeClass('d-none');
-
-    $('#takephoto').removeClass('d-block');
-    $('#takephoto').addClass('d-none');
-
-    $('#retakephoto').removeClass('d-none');
-    $('#retakephoto').addClass('d-block');
-
-    $('#uploadphoto').removeClass('d-none');
-    $('#uploadphoto').addClass('d-block');
-}
