cbf19aebf4603dbb5b01551f15e9b15efb9d087e
Leo Chien <leoch@example.com>
2024-07-16 12:08:19 +0800

    Add Netlify configuration and functions

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const app = express();
 app.use(cors());
 
 app.use('/api', createProxyMiddleware({
-    target: 'https://strabismusdetection-net.onrender.com', // 替换为你的 API URL
+    target: 'https://strabismusdetection-net.onrender.com/', // 替换为你的 API URL
     changeOrigin: true,
     pathRewrite: {
         '^/api': '', // 重写路径，使其与目标路径匹配
     },
 }));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log(`Proxy server running on port ${port}`);
 });
