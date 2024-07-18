c79ae56ccb7b6e541f4392084473762b7013b60f
Leo Chien <leoch@example.com>
2024-07-19 01:47:52 +0800

    Update dependencies versions

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,6 +9,19 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    fallback: {
+      "zlib": require.resolve("browserify-zlib"),
+      "querystring": require.resolve("querystring-es3"),
+      "path": require.resolve("path-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "fs": false,
+      "http": require.resolve("stream-http"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "url": require.resolve("url/")
+    }
+  },
   module: {
     rules: [
       {
