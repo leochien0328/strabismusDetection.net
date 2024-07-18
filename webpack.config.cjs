2f80a39ae24facd7f06e239af6b5f4f9b2f85991
Leo Chien <leoch@example.com>
2024-07-19 00:29:40 +0800

    Convert webpack.config.js to CommonJS syntax

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,6 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
