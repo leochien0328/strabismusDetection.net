df24d5af7cf0decec5fc04515569cce010ae4527
Leo Chien <leoch@example.com>
2024-07-19 00:11:57 +0800

    Add Netlify configuration and functions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 module.exports = {
   entry: './src/index.js',
