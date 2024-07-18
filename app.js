0e72a54e256ceda77786aa2f0d7f65472fce2a78
Leo Chien <leoch@example.com>
2024-07-18 12:14:58 +0800

    Add Netlify configuration and functions

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
+
+
 const app = express();
-app.use(cors());
+app.use(express.json()); // 解析 JSON 格式的请求体
+app.use(express.urlencoded({ extended: true })); // 解析 URL 编码的请求体
+app.use(cors({
+    origin: 'https://strabismusdetection.com'
+}));
 
 app.use('/api', createProxyMiddleware({
     target: 'https://strabismusdetection-net.onrender.com/', // 替换为你的 API URL
@@ -13,10 +19,20 @@ app.use('/api', createProxyMiddleware({
     },
 }));
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
     res.send('Hello World!');
 });
+app.post('/api/upload-photo', (req, res) => {
+    const { image } = req.body;
 
+    // 在這裡進行相應的處理，例如圖片處理、斜視檢測等等
+
+    // 返回處理結果，這裡假設您返回一個 JSON 對象
+    res.json({
+        message: 'Image uploaded and processed successfully',
+        result: 'your_result_here'
+    });
+});
 const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log(`Proxy server running on port ${port}`);
