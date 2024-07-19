b50398d4eb695be196a5179a41509b21b10a88d1
Leo Chien <leoch@example.com>
2024-07-19 14:44:05 +0800

    Update configurations and files for deployment

--- netlify/functions/upload-photo.js
+++ netlify/functions/upload-photo.js
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch'; // 导入 fetch 模块
 import express from 'express';
 const app = express();
-export async function upload_photo(raw_image_data){
+export async function handler(event) {
+    const raw_image_data = event.body;
     const apiUrl = process.env.API_URL || 'https://strabismusdetection-net.onrender.com/api/upload-photo';
 
     try {
