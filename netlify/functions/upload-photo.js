cabb6c327b90f52e31d2fb3be462cf0e55f7119d
Leo Chien <leoch@example.com>
2024-07-19 17:44:01 +0800

    Update configurations and files for deployment

--- netlify/functions/upload-photo.js
+++ netlify/functions/upload-photo.js
@@ -1,27 +1,32 @@
-import fetch from 'node-fetch'; // 导入 fetch 模块
-exports.handler = async (event) => {
+import fetch from 'node-fetch';
+
+export const handler = async (event) => {
+  try {
+    console.log('Event Body:', event.body);
     const raw_image_data = JSON.parse(event.body).image;
     const apiUrl = process.env.API_URL || 'https://strabismusdetection-net.onrender.com/api/upload-photo';
-  
-    try {
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': '70bdf7dde1abcefebd3f83b09656e340'
-        },
-        body: JSON.stringify({ image: raw_image_data })
-      });
-      const data = await response.json();
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data)
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Failed to upload photo' })
-      };
-    }
-  };
 
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': '70bdf7dde1abcefebd3f83b09656e340'
+      },
+      body: JSON.stringify({ image: raw_image_data })
+    });
+
+    const data = await response.json();
+    console.log('API Response:', data);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to upload photo' })
+    };
+  }
+};
