bec8ac29ba19977b7bbda55e4896d27c6b878b55
Leo Chien <leoch@example.com>
2024-07-21 10:03:20 +0800

    Update configurations and files for deployment

--- netlify/functions/upload-photo.js
+++ netlify/functions/upload-photo.js
@@ -2,12 +2,6 @@ import fetch from 'node-fetch';
 
 exports.handler = async (event) => {
     try {
-        console.log("Received event:", JSON.stringify(event));  // 打印整个事件对象
-
-        if (!event.body) {
-            throw new Error("Invalid request body");
-        }
-
         const { image } = JSON.parse(event.body);
 
         if (!image) {
@@ -26,13 +20,11 @@ exports.handler = async (event) => {
         });
 
         const data = await response.json();
-        console.log("Received response from API:", JSON.stringify(data));  // 添加日志
         return {
             statusCode: 200,
             body: JSON.stringify(data)
         };
     } catch (error) {
-        console.error("Error:", error.message);  // 添加日志
         return {
             statusCode: 500,
             body: JSON.stringify({ error: error.message || 'Failed to upload photo' })
